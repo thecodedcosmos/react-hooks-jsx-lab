87400a8f3da042293eaae0280381bf6c2394969c
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2024-09-24 23:45:56 -0700

    updated the div element forr home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div id="Home">Home</div>;
+  return <div id="Home">
+    <h1 style={{ color: "firebrick" }}>
+        Your Name is a Web Developer from {name} in {city}
+        </h1>
+  </div>;
 }
 
 export default Home;
