093eaea135ea97407c14c003c11622edc3833663
adriannahdbell@gmail.com <120890577+thecodedcosmos@users.noreply.github.com>
2024-09-25 00:11:05 -0700

    added parentheses in About

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id="about">
+  return (<div id="about">
     <h2>About Me</h2>
     <p>This is a brief description about myself.</p>
       <img src={image} alt="I made this" />
-        </div>;
+        </div>);
 }
 
 export default About;
